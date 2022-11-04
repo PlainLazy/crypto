6e26418703a033cf7c885e72c7b6d3987a67e8b1
Andrey Yakovlev <andr64@gmail.com>
2022-11-04 13:33:52 +0300

    Update xc_create_simlink_in_home.ps1

--- xc_create_simlink_in_home.ps1
+++ xc_create_simlink_in_home.ps1
@@ -1,6 +1,11 @@
 ﻿# create symlink of chia app
 # makes possible to use "~\chia\chia" instead of full path
 
-cd ~\AppData\Local\chia-blockchain\app-*\resources\app.asar.unpacked\daemon
+# version <= 1.6.0
+#cd ~\AppData\Local\chia-blockchain\app-*\resources\app.asar.unpacked\daemon
+
+# version >= 1.6.1
+cd ~\AppData\Local\Programs\Chia\resources\app.asar.unpacked\daemon
+
 New-Item -Path ~\chia -ItemType SymbolicLink -Value (Get-Location).Path -Force
 Pause
