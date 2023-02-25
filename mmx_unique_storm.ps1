db01c2d3ae443bade4865bc44b1dd47129380184
Andrey Yakovlev <andr64@gmail.com>
2023-02-25 21:09:37 +0300

    Update mmx_unique_storm.ps1

--- mmx_unique_storm.ps1
+++ mmx_unique_storm.ps1
@@ -1,34 +1,28 @@
 cd "c:\program files\mmx"
 
-# Will spam only the unique addresses.
-# Take the addresses starting from the last block, jump back through $blocks_delta blocks and so $scan_blocks times.
-# Ignore repeating addresses.
+$global:uniqueAddrs = @()
 
-$scan_blocks = 1000
-$blocks_delta = 30
-
-$height = ((./mmx node info | select-string "height:") -split ' ')[-1]
-write-host "current height $height"
-
-$addrs = @()
-
-for (($i = $scan_blocks), ($block = $height-1); $i -gt 0; ($block -= $blocks_delta), $i--) {
-	$a = ((./mmx node get block $block) | convertfrom-json).tx_base.outputs[1].address
-	$pc = [math]::floor(100-$i/$scan_blocks*100)
-	write-host "`rgetting addr $a from block $block ... $pc % " -NoNewline
-	if ($a -and !($addrs | where-object { $_ -eq $a })) {
-		$addrs += $a
+function Fetch-Random-Addr {
+	$currentHeight = ((./mmx node info | select-string "height:") -split ' ')[-1]
+	$randomBlock = (Get-Random -Minimum ($currentHeight - 86400) -Maximum $currentHeight)
+	$addr = ((./mmx node get block $randomBlock) | ConvertFrom-Json).tx_base.outputs[1].address
+	if ($addr -and !($global:uniqueAddrs | Where-Object { $_ -eq $addr })) {
+		$global:uniqueAddrs += $addr
+		Write-Host "unique addr $addr has been found in block $randomBlock, total addrs " $global:uniqueAddrs.length
 	}
 }
 
-write-host ""
-$u = $addrs.length
-write-host "collected $u unique addresses in $scan_blocks blocks, let's go:"
-
-while (1) {
-	$to = Get-Random -InputObject $addrs
-	./mmx wallet send -t $to -a (Get-Random -Minimum 0.001 -Maximum 0.002)
-	start-sleep -seconds (Get-Random -Minimum 9 -Maximum 18)
+for ($i = 0; $i -lt 100; $i++) {
+	Fetch-Random-Addr
 }
 
-pause
+While (1) {
+	Fetch-Random-Addr
+	$addr = Get-Random -InputObject $global:uniqueAddrs
+	if ($addr) {
+		./mmx wallet send -t $addr -a (Get-Random -Minimum 0.001 -Maximum 0.002)
+	}
+	Start-Sleep -Seconds (Get-Random -Minimum 8 -Maximum 16)
+}
+
+Pause
