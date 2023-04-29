e4a9cbc6b7ff931b68cebe70bab6160abfaec28a
Andrey Yakovlev <andr64@gmail.com>
2023-04-30 00:21:09 +0300

    Update mmx_unique_storm.ps1

--- mmx_unique_storm.ps1
+++ mmx_unique_storm.ps1
@@ -1,11 +1,14 @@
+# mmx node testnet10
+
 cd "c:\program files\mmx"
 
 $global:uniqueAddrs = @()
 
 function Fetch-Random-Addr {
 	$currentHeight = ((./mmx node info | select-string "height:") -split ' ')[-1]
-	$randomBlock = (Get-Random -Minimum ($currentHeight - 86400) -Maximum $currentHeight)
-	$addr = ((./mmx node get block $randomBlock) | ConvertFrom-Json).tx_base.outputs[1].address
+	$minHeight = (($currentHeight - 86400), 1 | Measure-Object -Maximum).Maximum
+	$randomBlock = [Math]::floor((Get-Random -Minimum $minHeight -Maximum $currentHeight))
+	$addr = ((./mmx node get block $randomBlock) | ConvertFrom-Json).reward_addr
 	if ($addr -and !($global:uniqueAddrs | Where-Object { $_ -eq $addr })) {
 		$global:uniqueAddrs += $addr
 		Write-Host "unique addr $addr has been found in block $randomBlock, total addrs " $global:uniqueAddrs.length
@@ -22,7 +25,7 @@ While (1) {
 	if ($addr) {
 		./mmx wallet send -t $addr -a (Get-Random -Minimum 0.001 -Maximum 0.002)
 	}
-	Start-Sleep -Seconds (Get-Random -Minimum 8 -Maximum 16)
+	Start-Sleep -Seconds (Get-Random -Minimum 5 -Maximum 7)
 }
 
 Pause
