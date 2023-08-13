fe1d108056cb67a5b0cb5832defdf112d31f5fd1
Andrey Yakovlev <andr64@gmail.com>
2023-08-13 19:54:26 +0300

    Create sm_node_XXXX_filter.ps1

---
+++ sm_node_XXXX_filter.ps1
@@ -0,0 +1,3 @@
+$host.ui.RawUI.WindowTitle = $MyInvocation.MyCommand.Name
+Get-Content log_NNNN.txt -Tail 10000 -Wait | Select-String 'proposal created|proposal eligibility for an epoch|generated block|ERROR|App version'
+Pause
