9db11ec6f27dd684af5d8e847429067980e38806
Andrey Yakovlev <andr64@gmail.com>
2023-08-13 21:50:38 +0300

    Update sm_node_XXXX_filter.ps1

--- sm_node_XXXX_filter.ps1
+++ sm_node_XXXX_filter.ps1
@@ -1,3 +1,3 @@
 $host.ui.RawUI.WindowTitle = $MyInvocation.MyCommand.Name
-Get-Content log_NNNN.txt -Tail 10000 -Wait | Select-String 'proposal created|proposal eligibility for an epoch|generated block|ERROR|App version'
+Get-Content log_NNNN.txt -Tail 10000 -Wait | Select-String 'proposal created|proposal eligibility for an epoch|generated block|failed to process hare output|ERROR|App version'
 Pause
