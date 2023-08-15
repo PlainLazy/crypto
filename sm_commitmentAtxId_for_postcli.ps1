d4901687a0194476d6f9ae6fd4f5dc94c72678d0
Andrey Yakovlev <andr64@gmail.com>
2023-08-15 16:40:11 +0300

    Create sm_commitmentAtxId_for_postcli.ps1

---
+++ sm_commitmentAtxId_for_postcli.ps1
@@ -0,0 +1,4 @@
+$idBase64 = (.\grpcurl.exe -plaintext -d "{}" localhost:9092 spacemesh.v1.ActivationService.Highest | ConvertFrom-Json).atx.id.id
+$idHex = (([System.Convert]::FromBase64String($idBase64) | ForEach-Object {"{0:X}" -f $_}) -Join '').ToLower()
+echo "-commitmentAtxId $idHex"
+Pause
