2598d419c815a20558040292fde5536cd3279d63
Andrey Yakovlev <andr64@gmail.com>
2023-08-15 16:41:58 +0300

    Update sm_commitmentAtxId_for_postcli.ps1

--- sm_commitmentAtxId_for_postcli.ps1
+++ sm_commitmentAtxId_for_postcli.ps1
@@ -1,3 +1,6 @@
+# get grpcurl here: https://github.com/fullstorydev/grpcurl/releases
+# check port 9092
+
 $idBase64 = (.\grpcurl.exe -plaintext -d "{}" localhost:9092 spacemesh.v1.ActivationService.Highest | ConvertFrom-Json).atx.id.id
 $idHex = (([System.Convert]::FromBase64String($idBase64) | ForEach-Object {"{0:X}" -f $_}) -Join '').ToLower()
 echo "-commitmentAtxId $idHex"
