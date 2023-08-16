cf22b8a88771607d8b86d293fdd55b9cebdfea9a
Andrey Yakovlev <andr64@gmail.com>
2023-08-16 15:09:55 +0300

    Update sm_node_XXXX_xxTiB.ps1

--- sm_node_XXXX_xxTiB.ps1
+++ sm_node_XXXX_xxTiB.ps1
@@ -2,28 +2,38 @@ $host.ui.RawUI.WindowTitle = $MyInvocation.MyCommand.Name
 
 # get lastest go-spacemesh here: https://github.com/spacemeshos/go-spacemesh/releases
 # get mainnet config here: https://configs.spacemesh.network/config.mainnet.json
-
-# change NNNN with yours (for example: use first 4 chars of your node_id)
-# smeshing-opts-provider 4294967295 <-- using CPU only
-# check datadir, maxfilesize, numunits
-# for multiple instances, replace ports (7514,9095,9096,9097) with ones that are not yet binded
+# rename config.mainnet.json to config_NNNN.json
+# add this into config_NNNN.json:
+<#
+    "smeshing": {
+        "smeshing-opts": {
+            "smeshing-opts-datadir": "D:\\path_to_post_data",
+            "smeshing-opts-maxfilesize": 17179869184,
+            "smeshing-opts-numunits": 19,
+            "smeshing-opts-provider": 4294967295,
+            "smeshing-opts-throttle": false,
+            "smeshing-opts-compute-batch-size": 1048576
+        },
+        "smeshing-coinbase": "your_wallet",
+        "smeshing-proving-opts": {
+            "smeshing-opts-proving-nonces": 288,
+            "smeshing-opts-proving-threads": 6
+        },
+        "smeshing-start": true
+    }
+#>
+# and check values of: datadir, maxfilesize, numunits, coinbase, nonces, threads
+# change NNNN with first 4 chars of your node_id
+# for multiple instances, replace ports (7514,9095,9096,9097 from config and parameters) with ones that are not yet binded
+# memo: provider 4294967295 <-- using CPU only
 
 while (1) {
 
     ./go-spacemesh `
-    --listen /ip4/0.0.0.0/tcp/7514 `
-    --grpc-public-listener 0.0.0.0:9095 `
-    --grpc-private-listener 127.0.0.0:9096 `
-    --grpc-json-listener 0.0.0.0:9097 `
-    --config config.mainnet.json `
+    --listen /ip4/0.0.0.0/tcp/7515 `
+    --config config_NNNN.json `
     --data-folder node_NNNN `
     --filelock lock_NNNN `
-    --smeshing-coinbase sm1qqq... `
-    --smeshing-start `
-    --smeshing-opts-provider 4294967295 `
-    --smeshing-opts-datadir C:\PATH_TO_POST_DATA `
-    --smeshing-opts-maxfilesize 4294967296 `
-    --smeshing-opts-numunits 4 `
     2>&1 | Tee log_NNNN.txt -Append
 
     Start-Sleep -Seconds 30
