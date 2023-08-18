7f3234a0c2a90e36bd312611a70423f683b82494
Andrey Yakovlev <andr64@gmail.com>
2023-08-18 22:30:11 +0300

    Update sm_node_XXXX_filter.ps1

--- sm_node_XXXX_filter.ps1
+++ sm_node_XXXX_filter.ps1
@@ -1,3 +1,3 @@
 $host.ui.RawUI.WindowTitle = $MyInvocation.MyCommand.Name
-Get-Content log_NNNN.txt -Tail 10000 -Wait | Select-String 'proposal created|proposal eligibility for an epoch|generated block|failed to process hare output|waiting till poet round end|ERROR|App version'
+Get-Content log_NNNN.txt -Tail 10000 -Wait | Select-String 'proposal created|proposal eligibility for an epoch|generated block|failed to process hare output|not starting hare: node not synced at this layer|waiting till poet round end|ERROR|App version'
 Pause
