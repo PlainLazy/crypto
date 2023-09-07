66575f771a1b185ab4c554c3625e1db4d18e2bc2
Andrey Yakovlev <andr64@gmail.com>
2023-09-07 20:15:46 +0300

    Create sm_total_supply.sql

---
+++ sm_total_supply.sql
@@ -0,0 +1,11 @@
+select
+	sum(t2.balance)/1000000000 total_supply
+from (
+	select
+		address a, max(layer_updated) l, count(*) c
+	from accounts
+	where layer_updated > 0
+	group by a
+) t1
+	join accounts t2
+	on t1.a = t2.address and t1.l = t2.layer_updated
