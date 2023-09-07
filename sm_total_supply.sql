c988da6d7b5aa15646171189bc83e2052a39ee16
Andrey Yakovlev <andr64@gmail.com>
2023-09-07 20:17:16 +0300

    Update sm_total_supply.sql

--- sm_total_supply.sql
+++ sm_total_supply.sql
@@ -2,7 +2,7 @@ select
 	sum(t2.balance)/1000000000 total_supply
 from (
 	select
-		address a, max(layer_updated) l, count(*) c
+		address a, max(layer_updated) l
 	from accounts
 	where layer_updated > 0
 	group by a
