f29adc2752b20d131796b3e7fdd43b8984e66601
andrey <andr64@gmail.com>
2024-02-21 18:47:02 +0300

    --

--- spacemesh/node_private_N/sqlite3.state.cache.ps1
+++ spacemesh/node_private_N/sqlite3.state.cache.ps1
@@ -1,8 +1,8 @@
 $host.ui.RawUI.WindowTitle = Split-Path -Path $pwd -Leaf
 [console]::WindowWidth  = 90;
 [console]::WindowHeight = 10;
-write-host "read state.sql ..."
+write-host "get hash of state.sql ..."
 $tStart = Get-Date
-$b = [System.IO.File]::ReadAllBytes("state.sql")
-write-host "readed $($b.count) bytes in $(((Get-Date) - $tStart).TotalSeconds) seconds"
+Get-FileHash state.sql | Format-List
+write-host "done in $(((Get-Date) - $tStart).TotalSeconds) seconds"
 pause
