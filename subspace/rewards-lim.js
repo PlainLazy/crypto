2e9ac9778ce4904d373de5ae22aa74fbb0722d49
andrey <andr64@gmail.com>
2024-11-29 16:57:25 +0300

    --

---
+++ subspace/rewards-lim.js
@@ -0,0 +1,15 @@
+// use in console https://astral.autonomys.xyz/
+(async function(acc,lim){
+    await fetch('//subql.green.mainnet.subspace.network/v1/graphql', {method:'POST', body:JSON.stringify({
+     operationName: "AccountById",
+     variables: {accountId:acc},
+     query: `query AccountById($accountId: String!) {consensus_rewards(limit: ${lim} order_by: {block_height: desc} where: {account_id: {_eq: $accountId}, amount: {_gt: 0}}) {rewardType: reward_type amount}}`})
+    }).then(async r => {
+     const rews = (await r.json()).data.consensus_rewards
+     const rnd = (v) => Math.round(v*100)/100
+     const sum = (l) => rnd(l.reduce((a,v)=>a+Number(v.amount),0)/1e18)
+     const blks = rews.filter(t => t.rewardType!=="rewards.VoteReward")
+     console.log(`Received in the last ${rews.length} rewards: ${sum(rews)}(AI3), including ${sum(blks)}(AI3) from ${blks.length}(${rnd(blks.length/rews.length*100)}%) blocks`)
+    })
+  })
+  ('sudAj4uWBT6NSNUURqyDGGpFd7RvyytpLszfKuDtJovBrzmbG', 15000)
